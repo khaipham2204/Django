bf013f0479d5b8bc2bd5d65147eb27051346f87e
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-09-17 22:57:36 +0700

    Update schema.py

--- GraphQL/schema.py
+++ GraphQL/schema.py
@@ -1,1 +1,14 @@
+import graphene
+from . import schema_contact, schema_person
 
+class Query(schema_contact.Query, schema_person.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+class Mutation(schema_contact.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Mutation
+    # as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
