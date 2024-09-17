e3cc02e80fa5b206704d0d0a34a061ef267ddaa5
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-09-17 23:20:55 +0700

    Update schema_person.py

--- GraphQL/schema_person.py
+++ GraphQL/schema_person.py
@@ -1,25 +1,35 @@
 from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-
 from .models import Person
 
 class PersonNode(DjangoObjectType):
-  '''
-  Relay with Graphene-Django gives us some additional features:
-  - Pagination and slicing.
-  - An abstract id value which contains enough info for the server to know its type and its id.
-  ...
-  
+    '''
+        Relay with Graphene-Django gives us some additional features:
+        - Pagination and slicing.
+        - An abstract id value which contains enough info for the server to know its type and its id.
+    '''
+    
     class Meta:
         model = Person
         filter_fields = {
             'full_name': ['exact', 'icontains', 'istartswith'],
             'address': ['exact', 'icontains'],
         }
-
         interfaces = (relay.Node, )
+        fields = "__all__"
+        
+class PersonConnection(relay.Connection):
+    class Meta:
+        node = PersonNode       
         
 class Query(ObjectType):
-    person = relay.Node.Field(PersonNode)
-    all_persons = DjangoFilterConnectionField(PersonNode) 
+    person_none = relay.Node.Field(PersonNode)
+    
+    all_persons = DjangoFilterConnectionField(PersonNode)
+    
+    person_conn = relay.ConnectionField(PersonConnection)
+    
+    def resolve_person_conn(root, info, **kwargs):
+        return Person.objects.all()
+    
