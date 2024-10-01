4e66ba99784564854797536e0e868cafa8e14d47
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-10-01 16:59:56 +0700

    Update test_consumer_connections.py

--- Channels/tests/test_consumer_connections.py
+++ Channels/tests/test_consumer_connections.py
@@ -1,1 +1,10 @@
+import pytest
+from channels.testing import HttpCommunicator
+from consumers import MyConsumer
 
+@pytest.mark.asyncio
+async def test_my_consumer():
+    communicator = HttpCommunicator(MyConsumer.as_asgi(), "GET", "/test/")
+    response = await communicator.get_response()
+    assert response["body"] == b"test response"
+    assert response["status"] == 200
