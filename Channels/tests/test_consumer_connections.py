f252e7c0a29e5fe3943f859d454878e1bbc8837b
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-10-03 10:46:52 +0700

    Update test_consumer_connections.py

--- Channels/tests/test_consumer_connections.py
+++ Channels/tests/test_consumer_connections.py
@@ -1,10 +1,23 @@
 import pytest
-from channels.testing import HttpCommunicator
-from consumers import MyConsumer
+from channels.testing import WebsocketCommunicator
+from core.asgi import application
 
 @pytest.mark.asyncio
-async def test_my_consumer():
-    communicator = HttpCommunicator(MyConsumer.as_asgi(), "GET", "/test/")
-    response = await communicator.get_response()
-    assert response["body"] == b"test response"
-    assert response["status"] == 200
+async def test_websocket_connection():
+    # Communicator
+    communicator = WebsocketCommunicator(application, "/ws/chat/")
+    
+    # Connected:
+    connected, _ = await communicator.connect()
+    assert connected
+    
+    # Send messages:
+    message = {"message":"hi"}
+    await communicator.send_json_to(message)
+    
+    # Receive messages:
+    response = await communicator.receive_json_from()
+    assert response == {"message":"hi"}
+    
+    # Close the WebSocket connection
+    await communicator.disconnect()
