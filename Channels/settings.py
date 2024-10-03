3c32be6336161848e0e8d54ceaeb80caef2754b8
Khai PHAM (Mr.Kay) <khaipham2204@gmail.com>
2024-10-03 10:04:21 +0700

    Update settings.py

--- Channels/settings.py
+++ Channels/settings.py
@@ -1,1 +1,12 @@
+# Channels - daphne
+ASGI_APPLICATION = 'core.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
